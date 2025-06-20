b786ffad29eddea1b5e710b2d022cbb3234301c8
Georgia <georgialloydphoto@gmail.com>
2025-06-20 09:59:11 -0500

    Edit header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import "./Header.css";
-import logo from "../../assets/branding/morsel-large-logo-dark.svg";
+import logo from "../../assets/branding/morsel-fork-logo-large.svg";
 
 function Header() {
   return (
     <header className="header">
-        <img src={logo} alt="Morsel Logo" className="header__logo" />
-        <button className="header__button">ABOUT About</button>
+      <img src={logo} alt="Morsel Logo" className="header__logo" />
+      <div className="header__navigation-container">
+        <button className="header__button">About</button>
+        <button className="header__button">Services</button>
+        <button className="header__button">Contact</button>
+      </div>
     </header>
   );
 }
