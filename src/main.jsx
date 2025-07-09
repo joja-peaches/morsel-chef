d6c96f00394e98e15cf3a2678fbe21cc46713a53
Georgia <georgialloydphoto@gmail.com>
2025-07-09 00:40:39 -0500

    Forget about github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./components/App/App.jsx";
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
